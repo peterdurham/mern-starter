d459314d99405184eda4a3c22e6542a929b0fbad
Peter Durham <peterdurham@protonmail.com>
2020-05-24 08:07:05 -0700

    adjusted heroku scripts

--- src/server/index.js
+++ src/server/index.js
@@ -34,7 +34,7 @@ app.get("/api/getUsername", (req, res) =>
 
 app.use(express.static(path.join(__dirname, "dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist"));
+  res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
 app.listen(process.env.PORT || 8080, () =>
